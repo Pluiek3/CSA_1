3f65fc2bc29ac49474bea330b6d3dfc7e4dab5d5
Pluiek3 <pluiek.x3@gmail.com>
2026-09-15 23:40:32 +0300

    feat: add result output

--- lab1.asm
+++ lab1.asm
@@ -15,9 +15,21 @@ includelib msvcrt.lib
 	dqq DQ 171.233
 .CODE
 START:
-	XOR EAX, EAX
-	MOV AX, a
+	XOR EAX, EAX	     	
+	MOV AX, a	      
 	MOV EBX, b
 	ADD EAX, EBX
 	MOV r, EAX
+	
+        ; --- Вывод результата ---
+        push r                ; второй аргумент: значение r
+        push offset printf    ; первый аргумент: адрес строки формата
+        call crt_printf       ; вызов printf("Result: %d", r)
+        add esp, 8            ; очистка стека (2 аргумента по 4 байта)
+
+        ; --- Задержка, чтобы увидеть результат ---
+        call crt__getch       ; ожидание нажатия клавиши (getch)
+
+        push 0
+        call ExitProcess  
 END START
